28ab6943407156bd8174d1b70196539f520dde34
Sampson <297511521@qq.com>
2023-05-20 12:25:58 +0800

    Update imx.sh

--- top1/imx.sh
+++ top1/imx.sh
@@ -1,1 +1,1 @@
-python bot.py --exchange bitget  --strategy bitget_hedge_dynamic --config t105_config.json --symbol IMXUSDT_UMCBL
+python bot.py --exchange bitget  --strategy bitget_hedge_dynamic --config t106_config.json --symbol IMXUSDT_UMCBL
